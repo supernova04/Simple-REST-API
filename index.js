f1dbb998a820ef69d356345dfb1b9f9d9745f991
adm <timothyharlian99@gmail.com>
2022-09-12 19:26:07 +0700

    Node.JS Simple REST API

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const port = process.env.port
 
 //JSON response to root endpoint
 app.get('/', (req, res) => {
