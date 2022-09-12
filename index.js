a16859fa38811162e157d452c49e78c438b318b1
adm <timothyharlian99@gmail.com>
2022-09-12 18:04:19 +0700

    Node.JS Simple REST API

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8080 //apps running at port 9000
+const port = 9000 //apps running at port 9000
 
 //JSON response to root endpoint
 app.get('/', (req, res) => {
